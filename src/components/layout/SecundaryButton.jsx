507abfc1a31abb5212db7dce03b8cc8f7bed5245
Eduardo Cardoso de J. <94879340+DuhCardoso@users.noreply.github.com>
2026-01-18 22:08:25 -0300

    Update: correção do compente "SecundaryButton.jsx"

--- src/components/layout/SecundaryButton.jsx
+++ src/components/layout/SecundaryButton.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SecundaryButton = ({ href, content }) => {
   return (
-    <a href="#contact">
-      <button className="px-8 py-3 border-2 border-cyan-400 text-cyan-400 font-bold rounded-lg hover:bg-cyan-400 hover:text-slate-900 transition-all duration-300">
-        Download CV
+    <a href={href}>
+      <button className="px-8 py-[0.68rem] border-2 border-cyan-400 text-cyan-400 font-bold rounded-lg hover:bg-cyan-400 hover:text-slate-900 transition-all duration-300 transform hover:scale-105">
+        {content}
       </button>
     </a>
   );
