f2796e03cde8014d18674e4929d60e18c1f16129
Eduardo Cardoso de J. <94879340+DuhCardoso@users.noreply.github.com>
2026-01-21 00:25:21 -0300

    Update: correção do src do "Profile.jsx"

--- src/components/pages/About/Component/Profile.jsx
+++ src/components/pages/About/Component/Profile.jsx
@@ -8,7 +8,7 @@ const Profile = () => {
       <div className="relative w-80 h-80 rounded-xl overflow-hidden border-5 border-cyan-400/30 bg-linear-to-br from-cyan-400/10 to-blue-500/10">
         <div className="absolute inset-0 bg-linear-to-br from-cyan-400 to-blue-500 opacity-6"></div>
         <img
-          src="../../../../public/profile-placeholder.jpeg"
+          src="/profile-placeholder.jpeg"
           alt="Profile"
           className="flex items-center justify-center text-6xl font-bold text-cyan-400"
         />
