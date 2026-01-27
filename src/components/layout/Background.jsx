b8d2adc6b2442278625adbb6b251c4cb698617e7
Eduardo Cardoso de J. <94879340+DuhCardoso@users.noreply.github.com>
2026-01-26 21:47:54 -0300

    Update: Ajustes no BG

--- src/components/layout/Background.jsx
+++ src/components/layout/Background.jsx
@@ -8,13 +8,14 @@ const Background = () => {
 
       {/* Animation */}
       <div className="fixed top-0 left-0 w-full h-full overflow-hidden -z-20 pointer-events-none flex justify-center items-center filter blur-[100px] opacity-50">
-        <div className="bg-violet-500 w-87.5 h-62.5 rounded-full absolute animate-movinBg rotate-45"></div>
-        <div className="bg-blue-500 w-90 h-65 rounded-full absolute animate-movinBg rotate-90"></div>
-        <div className="bg-cyan-500 w-92.5 h-67.5 rounded-full absolute animate-movinBg "></div>
-        <div className="bg-indigo-500 w-95 h-70 rounded-full absolute animate-movinBg rotate-180"></div>
-        <div className="bg-purple-500 w-97.5 h-72.5 rounded-full  absolute animate-movinBg -rotate-45"></div>
-        <div className="bg-teal-500 w-100 h-75 rounded-full absolute animate-movinBg -rotate-90"></div>
-        <div className="bg-sky-500 w-102.5 h-77.5 rounded-full  absolute animate-movinBg -rotate-180"></div>
+        <div className="bg-violet-500/55 w-87.5 h-62.5 rounded-full absolute animate-movinBg rotate-45"></div>
+        <div className="bg-blue-500/55 w-90 h-65 rounded-full absolute animate-movinBg rotate-90"></div>
+        <div className="bg-cyan-500/55 w-92.5 h-67.5 rounded-full absolute animate-movinBg rotate-135"></div>
+        <div className="bg-sky-500/55 w-92.5 h-67.5 rounded-full absolute animate-movinBg "></div>
+        <div className="bg-purple-500/55 w-97.5 h-72.5 rounded-full  absolute animate-movinBg -rotate-45"></div>
+        <div className="bg-teal-500/55 w-100 h-75 rounded-full absolute animate-movinBg -rotate-90"></div>
+        <div className="bg-sky-500/55 w-102.5 h-77.5 rounded-full  absolute animate-movinBg -rotate-135"></div>
+        <div className="bg-indigo-500/55 w-102.5 h-77.5 rounded-full  absolute animate-movinBg -rotate-180"></div>
       </div>
     </div>
   );
